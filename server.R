c9e4243170c6011e27e657bc7f05c872c3b6ac82
tanguydlz <59443450+tanguydlz@users.noreply.github.com>
2021-03-08 17:06:18 +0100

    v2

--- server.R
+++ server.R
@@ -1,23 +1,103 @@
-server <- function(input, output, session) {
+server <- function(input, output) {
   
   setwd("C:/Users/tdelzantda/Downloads")
   df = read.csv2("logs_fw-3.csv", header=T, sep=";")
-  
+  colnames(df)[1] = "id"
   
     #Word plot data
     proto <- reactive({
-      port = df %>% filter(proto=="")
-
+      if(as.numeric(input$port)==1){
+        ports=0:65535
+      }else if(as.numeric(input$port)==2){
+        ports=0:1023
+      }else if(as.numeric(input$port)==3){
+        ports=1024:49151
+      }else if(as.numeric(input$port)==4){
+        ports=49152:65535
+        }
+      port = df %>% filter(proto %in% input$proto) %>% filter(action %in% input$action) %>% filter(dstport %in% ports )
+      
     })
   
+    actu <- reactive({
+      tab=df %>% filter(proto %in% input$proto2) %>% filter(action %in% input$action2)
+      tab=as.data.frame(tab)
+    })
     
-      #word frequency barplot
-      output$freqPlot <- renderPlot({
-        freq <- proto()
-        new_df <- freq[1:input$numWords,]
-        ggplot(new_df, aes(x=reorder(word, n), y=n, fill = as.factor(word))) +
-          geom_bar(stat = "identity", position = "dodge", col= "black") + xlab("Mots") + ylab("Nombre") +
+    ip <- reactive({
+      ip = df %>% filter(proto %in% input$proto4) %>% filter(action %in% input$action4) %>% count(ipsrc,sort=T) %>% top_n(5)
+    })
+    
+    topPorts <- reactive({
+      ip = ip()
+      p=df %>% filter(ipsrc %in% ip$ipsrc)
+      p2 = p %>% count(ipsrc,dstport,  sort=T) %>% top_n(10)
+    })
+    
+    
+    
+
+    portinf <- reactive ({
+      portA = df %>% filter(dstport < 1024 & action=="PERMIT") %>% count(dstport, sort=T) %>% top_n(10)
+    })
+    
+    hues <- c(60:330)
+###################################################
+###################################################   
+###################################################   
+    
+    
+    output$freqProto <- renderPlot({
+        X <- proto()
+        X = X %>% count(proto)
+        ggplot(X, aes(x=proto, y=n, fill = as.factor(proto))) +
+          geom_bar(stat = "identity", position = "dodge", col= "black") + xlab("protocole") + ylab("Nombre") +
           coord_flip() + scale_fill_hue(c = sample(hues, 1)) + guides(fill=FALSE)
       })
+    
+    output$pieProto <- renderPlot({
+      X <- proto()
+      X = X %>% count(proto) %>% mutate(pourcentage = round(n/sum(n)*100,2))
+      ggplot(X, aes(x="", y=n, fill=proto)) +
+        geom_bar(stat="identity", width=1, color="white") + 
+        coord_polar("y", start=0) +
+        theme_void() +
+        geom_text(aes(y = n/3 + c(0, cumsum(n)[-length(n)]), 
+                      label = pourcentage), size=5)
+      
+    })
+    
+    output$tabProto  <- DT::renderDataTable({
+      X <- proto()
+      X = X %>% select(id, datetime, ipsrc, ipdst, dstport, policyid)
+      X
+    })
+    
+    output$tabData  <- DT::renderDataTable({
+      df = actu()
+      df
+      
+    })
+    
+    output$plotTop <- renderPlot({
+      ip = ip()
+      ggplot(ip, aes(x=ipsrc, y=n, fill = as.factor(ipsrc))) +
+        geom_bar(stat = "identity", position = "dodge", col= "black") + xlab("Ip Source") + ylab("Nombre") +
+        coord_flip() + scale_fill_hue(c = sample(hues, 1)) + guides(fill=FALSE)
+    })
+    
+    output$plotTop10 <- renderPlot({
+      port = portinf()
+      ggplot(port, aes(x=dstport, y=n, fill = as.factor(dstport), label=dstport)) +
+        geom_bar(stat = "identity", position = "dodge", col= "black") + xlab("Port") + ylab("Nombre") +
+        coord_flip() + scale_fill_hue(c = sample(hues, 1)) + guides(fill=FALSE) +
+        geom_text(size = 3, position = position_stack(vjust = 0.5))
+    })
+    
+    output$tabport <- renderTable({
+      topPorts <- topPorts()
+      topPorts
+    })
+    
   
 }
--- ui.R
+++ ui.R
@@ -1,11 +1,15 @@
 #install.packages("tidyverse")
+#install.packages("DT")
 library(shiny)
 library(shinyWidgets)
 library(shinydashboard)
 library(tidyverse)
+library(DT)
+library(scales)
 
 
 ui <- fluidPage(
+  
   titlePanel("Projet"),
   
   
@@ -31,19 +35,66 @@ ui <- fluidPage(
                        tags$div("Loading...",id="loadmessage")),
       #side panel 1
       conditionalPanel(condition="input.tabselected==1",
-                       helpText("Analyse des ports UDP et TCP"),
+                       helpText("Analyse des protocoles UDP et TCP"),
                        hr(),
-                      checkboxGroupInput("port", label = h3("Port"), 
-                         choices = list("UDP" = 1, "TCP" = 2),
-                         selected = 1))),
+                      checkboxGroupInput("proto", label = h3("Protocole"), 
+                         choices = c("UDP", "TCP"), selected=c("TCP","UDP")),
+                      checkboxGroupInput("action", label = h3("Action"), 
+                                         choices = c("PERMIT", "DENY"), selected=c("PERMIT","DENY")),
+                      radioButtons("port", label = h3("RFC 6056"), 
+                                         choices = list("Tout"=1,"The Well-Known Ports" =2,"The Registered Ports" = 3, "The Dynamic and/or Private Ports" =4), 
+                                         selected=1)),
+      
+      conditionalPanel(condition="input.tabselected==2",
+                       helpText("Analyse des données"),
+                       hr(),
+                       checkboxGroupInput("proto2", label = h3("Protocole"), 
+                                          choices = c("UDP", "TCP"), selected=c("TCP","UDP")),
+                       checkboxGroupInput("action2", label = h3("Action"), 
+                                          choices = c("PERMIT", "DENY"), selected=c("PERMIT","DENY"))),
+                       
+      conditionalPanel(condition="input.tabselected==3",
+                       helpText("Visualisation intéractive des données"),
+                       hr(),
+                       checkboxGroupInput("proto3", label = h3("Protocole"), 
+                                          choices = c("UDP", "TCP"), selected=c("TCP","UDP")),
+                       checkboxGroupInput("action3", label = h3("Action"), 
+                                          choices = c("PERMIT", "DENY"), selected=c("PERMIT","DENY"))),
+      
+      conditionalPanel(condition="input.tabselected==4",
+                       helpText("statistiques relatives au TOP 5 des IP sources les plus émettrices, TOP 10 des ports inférieurs à 1024 avec un accès autorisé"),
+                       hr(),
+                      checkboxGroupInput("proto4", label = h3("Protocole"), 
+                                         choices = c("UDP", "TCP"), selected=c("TCP","UDP")),
+                      checkboxGroupInput("action4", label = h3("Action"), 
+                                         choices = c("PERMIT", "DENY"), selected=c("PERMIT","DENY")))
+              ), #sidebar panel,
       
       mainPanel(
         tabsetPanel(
-          tabPanel("Analyse des ports", plotOutput("freqPlot"), value = 1,  
+          tabPanel("Analyse des ports", plotOutput("freqProto"), 
+                   fluidRow(
+                     column(6,plotOutput("pieProto")),
+                     column(6,DT::dataTableOutput("tabProto"))),
+                   value = 1,  
                    conditionalPanel(condition="input.tabselected==1")),
+          tabPanel("Analyse des données",
+                   DT::dataTableOutput("tabData"),
+                   value = 2,  
+                   conditionalPanel(condition="input.tabselected==2")),
+          tabPanel("Visualisation intéractive",
+                   value = 3,  
+                   conditionalPanel(condition="input.tabselected==3")),
+          tabPanel("TOP IP/PORT",
+                   fluidRow(
+                     column(6,plotOutput("plotTop")),
+                     column(6,plotOutput("plotTop10"))),
+                   tableOutput("tabport"),
+                   value = 4,  
+                   conditionalPanel(condition="input.tabselected==4")),
           id = "tabselected")
-      )
-    )
+      ) #Main panel
+  ) #Sidebar layout
   
-)
+) #fluidpage
 
